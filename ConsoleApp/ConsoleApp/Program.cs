1b47c4adbbb1d2daf8ea904d3b78c974f009df76
Piotr <piotrb770@wp.pl>
2024-11-29 21:32:58 +0100

    Modyfikacja 3

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine("Modyfikacja 1");
 // Correcting commit message
 Console.WriteLine("Modyfikacja 2");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Modyfikacja 3 - iteration {i + 1}");
+}
